c8b47b0f210de6e40afb224b6d283d5580f6b83d
mrjonc <jonathanlaureano33.jl@gmail.com>
2026-04-27 18:18:12 -0300

    modelando a navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,30 +1,38 @@
 import { Link, Outlet } from "react-router-dom";
-import { CiSearch } from "react-icons/ci";
+import { FaSearch } from "react-icons/fa";
+import { BiCameraMovie } from "react-icons/bi";
+import styles from "./NavBar.module.css";
 
 const NavBar = () => {
   return (
     <>
-      <section>
-        <nav id="navbar">
-          <Link to="/">
-            <h1>Library Movies</h1>
-          </Link>
+      <section className={styles.container}>
+        <nav className={styles.navbar}>
+          <div>
+            <Link to="/" className={styles.logoLink}>
+              <h1 className={styles.logo}>
+                <BiCameraMovie />
+                Library Movies
+              </h1>
+            </Link>
+          </div>
 
-          <form>
-            <input
-              type="text"
-              name="searchMovie"
-              id="searchMovie"
-              placeholder="Pesquisar"
-            />
-            <button type="submit">
-              <CiSearch />
-            </button>
-          </form>
-
-          <Outlet />
+          <div>
+            <form className={styles.searchForm}>
+              <input
+                type="text"
+                name="searchMovie"
+                id="searchMovie"
+                placeholder="Pesquisar"
+              />
+              <button type="submit">
+                <FaSearch />
+              </button>
+            </form>
+          </div>
         </nav>
       </section>
+      <Outlet />
     </>
   );
 };
