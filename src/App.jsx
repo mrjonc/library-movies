ddd61ec638bd6328fe3bc0e70ab1232a881c820c
mrjonc <jonathanlaureano33.jl@gmail.com>
2026-05-06 01:27:43 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,13 @@ import { Outlet, ScrollRestoration } from "react-router-dom";
 import { useState } from "react";
 import "./App.css";
 import NavBar from "./components/NavBar.jsx";
-import Home from "./pages/Home/Home.jsx";
-import Movie from "./pages/Movie/Movie.jsx";
-import Search from "./pages/Search/Search.jsx";
 
 function App() {
   const [page, setPage] = useState("home");
   return (
     <>
-      <NavBar setPage={setPage} />
-      {setPage === "Home" && <Home />}
-      {setPage === "Movie" && <Movie />}
-      {setPage === "Search" && <Search />}
+      <NavBar />
+
       <Outlet />
       <ScrollRestoration />
     </>
