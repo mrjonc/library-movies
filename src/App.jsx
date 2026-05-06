bb8fee903aa871f2b36682e4de02d1732c132926
mrjonc <jonathanlaureano33.jl@gmail.com>
2026-05-06 01:24:04 -0300

    Configurando as rotas no vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { Outlet, ScrollRestoration } from "react-router-dom";
+import { useState } from "react";
 import "./App.css";
 import NavBar from "./components/NavBar.jsx";
+import Home from "./pages/Home/Home.jsx";
+import Movie from "./pages/Movie/Movie.jsx";
+import Search from "./pages/Search/Search.jsx";
 
 function App() {
+  const [page, setPage] = useState("home");
   return (
     <>
-      <NavBar />
+      <NavBar setPage={setPage} />
+      {setPage === "Home" && <Home />}
+      {setPage === "Movie" && <Movie />}
+      {setPage === "Search" && <Search />}
       <Outlet />
       <ScrollRestoration />
     </>
